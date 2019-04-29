2414d8e4d4b427b46c24acf7e6eb8e33663dd86a
Ian Denegri <iandenegri@Hotmail.com>
2019-04-29 00:33:48 -0400

    Working on expanding the models for the site.
    Need to also clean up the media files at some point. Added support for the models in the admin panel.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Social, WebsiteSection, Project, Interest
 
 # Register your models here.
+
+admin.site.register(Social)
+admin.site.register(WebsiteSection)
+admin.site.register(Project)
+admin.site.register(Interest)
--- website/models.py
+++ website/models.py
@@ -2,7 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Socials(models.Model):
-    social_site = models.CharField()
+class Social(models.Model):
+    social_site = models.CharField(max_length=128)
     url = models.URLField()
     image = models.ImageField()
+
+    def __str__(self):
+        return self.social_site
+
+class WebsiteSection(models.Model):
+    section_name = models.CharField(max_length=128)
+    section_blurb = models.TextField()
+
+    def __str__(self):
+        return self.section_name
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=128)
+    URL = models.URLField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.project_name
+
+class Interest(models.Model):
+    interest_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.interest_name
