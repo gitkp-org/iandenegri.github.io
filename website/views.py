4d2dd131d480df6d1135658858e96b58483417ec
Ian Denegri <iandenegri@Hotmail.com>
2019-05-01 01:30:16 -0400

    HTML template is ready to have styling applied to it.

--- website/views.py
+++ website/views.py
@@ -5,7 +5,16 @@ from .models import Social, WebsiteSection, Project, Interest
 
 def home(request):
     interests = Interest.objects.all()
+    print(interests)
+    socials = Social.objects.all()
+    sections = WebsiteSection.objects.all()
+    print(sections)
+    projects = Project.objects.all()
+    print(projects)
     context = {
-        "interests": interests
+        "interests": interests,
+        "socials": socials,
+        "sections": sections,
+        "projects": projects
     }
     return render(request, "website/home.html", context=context)
