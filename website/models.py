39ae4bc2e8e3ed552188fd732e8d0bc4fb28a2f9
Ian Denegri <iandenegri@Hotmail.com>
2019-05-04 03:25:56 -0400

    Progress :D Site is looking more like it should.
    Still need to figure out how to host my resume.

--- website/models.py
+++ website/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
 
 class Social(models.Model):
     social_site = models.CharField(max_length=128)
-    url = models.URLField()
-    image = models.ImageField()
+    url = models.CharField(max_length=128)
+    image = models.ImageField(upload_to="social_icons")
 
     def __str__(self):
         return self.social_site
+
+    def save(self, *args, **kwargs):
+        super(Social, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 class WebsiteSection(models.Model):
     section_name = models.CharField(max_length=128)
