12bc7e5e5808c18588701156f6d312c8b6ca5db4
Ian Denegri <iandenegri@Hotmail.com>
2019-05-10 22:48:43 -0400

    changes to prevent media files from being pushed.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'safe-castle-34949.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'iandenegri.com']
 
 
 # Application definition
