046afb711b3c87f516b306ddb979c426d64a7377
Ian Denegri <iandenegri@Hotmail.com>
2019-05-13 01:37:41 -0400

    S3 set up properly now.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -140,5 +140,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = True # So you can upload resumes with the same file name and overwrite your old one.
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
