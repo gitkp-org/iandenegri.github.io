b1b7af64b3c42f5d8b1fb2655f923c8f3623793b
Ian Denegri <iandenegri@hotmail.com>
2019-05-18 22:45:51 -0400

    Better code design (I think?) :D

--- website/views.py
+++ website/views.py
@@ -7,8 +7,10 @@ from .models import Social, WebsiteSection, Project, Interest, Owner
 # Create your views here.
 
 def home(request):
-    website_parts = build_website()
-    owner = find_me("ian", "denegri")  # Ideally only you should be using this so you can just replace my first and last name with your own.
+    website = WebsiteUtility("ian", "denegri")  # Ideally only you should be using this so you can just replace my first and last name with your own.
+    website_parts = website.build_website()
+    owner = website.find_me()
+
     context = {
         "interests": website_parts['interests'],
         "socials": website_parts['socials'],
@@ -18,41 +20,44 @@ def home(request):
     }
     return render(request, "website/home.html", context=context)
 
-def find_me(first_name, last_name, capitilize=True):
+class WebsiteUtility:
     """
-    This function takes a first name and last name argument to look you up.
-    This is mainly to make the project a little simpler and easier to use.
-    EXAMPLE:
-    print(find_me("ian", "denegri"))
-    This will look through your Owner objects for "Ian" "Denegri"
-    If you want to look for your first and last name in all lower case characters then set "capitilize" to False.
-    EXAMPLE:
-    print(find_me("ian", "denegri", capitilize=False))
-    This will look through your Owner objects for "ian" "denegri"
+    This class contains the functions that are used to build the data needed to populate the site.
+    All you need to do is provide your first and last name. :-)
     """
-    if capitilize:
-        first = str(first_name).capitalize()
-    else:
-        first = str(first_name)
-    if capitilize:
-        last = str(last_name).capitalize()
-    else:
-        last = str(last_name)
-    try: 
-        myself = Owner.objects.filter(first_name=first).filter(last_name=last).get()
-        return myself
-    except ObjectDoesNotExist:
-        print("The user you're looking for doesn't exist! Did you enter your first and last name properly? \n\
-If you did, then are you sure that you created your Owner object in the Owner model table?")
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-def build_website():
-    """
-    This function will build a dictionary that contains all the parts needed to build your website.
-    This function assumes you have objects to display.
-    """
-    website_parts = dict()
-    website_parts['interests'] = Interest.objects.all().order_by('pk')
-    website_parts['socials'] = Social.objects.all().order_by('pk')
-    website_parts['sections'] = WebsiteSection.objects.all().order_by('pk')
-    website_parts['projects'] = Project.objects.all().order_by('pk')
-    return website_parts
+    def find_me(self, capitilize=True):
+        """
+        Finds you based on the first and last name you used when you instantiated the WebsiteUtility class.
+        If your name needs to be not capitilized then please add "capitilize=False" to your find_me() arguments. :-)
+        """
+        if capitilize:
+            first = str(self.first_name).capitalize()
+        else:
+            first = str(self.first_name)
+        if capitilize:
+            last = str(self.last_name).capitalize()
+        else:
+            last = str(self.last_name)
+        try: 
+            myself = Owner.objects.filter(first_name=first).filter(last_name=last).get()
+            return myself
+        except ObjectDoesNotExist:
+            print("The user you're looking for doesn't exist! Did you enter your first and last name properly? \n\
+                If you did then please make sure the capitilzation matches and if your name should be in lower case then please add 'capitilize=False' to the find_me() function as an argument. \
+                Additionally, then are you sure that you created your Owner object in the Owner model table?")
+
+    def build_website(self):
+        """
+        This function will build a dictionary that contains all the parts needed to build your website.
+        This function assumes you have objects to display.
+        """
+        website_parts = dict()
+        website_parts['interests'] = Interest.objects.all().order_by('pk')
+        website_parts['socials'] = Social.objects.all().order_by('pk')
+        website_parts['sections'] = WebsiteSection.objects.all().order_by('pk')
+        website_parts['projects'] = Project.objects.all().order_by('pk')
+        return website_parts
