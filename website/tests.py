42a18566ed0cdbc2754acf83e0dd3d4afc9b1c17
Ian Denegri <iandenegri@Hotmail.com>
2019-05-19 00:28:56 -0400

    Setting up imports for tests.

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,14 @@
+# Django
+from django.core.exceptions import (
+    ObjectDoesNotExist,
+    MultipleObjectsReturned,
+)
 from django.test import TestCase
 
+# Local
+from .views import WebsiteUtility
+from .models import Social, WebsiteSection, Project, Interest, Owner
+
 # Create your tests here.
+class TestWebsiteModels(TestCase):
+    pass
